e8f52a3e7b05c83c63470d58f4fd3fb11e46f47b
drwhitfield <43507455+drwhitfield@users.noreply.github.com>
2021-09-06 16:56:19 +0200

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Script to download urls from specified file
+"""
+
+__author__      =           'Donald Whitfield'
+__copyright__   =           '(c) 2021, S-Box Security'
+__version__     =           '1.0.0'
+__maintainer__  =           'Donald Whitfield'
+__email__       =           'donaldwhitfield@icloud.com'
+__status__      =           'Development'
+
+import subprocess
+
+file_list = list()
+file = ""
+
+cmd = '/usr/local/bin/youtube-dl'
+#run_command = subprocess.call([cmd, file])
+
+urlfile = open ("url_list.txt", "r")
+file_list = urlfile.readlines()
+
+for file in file_list:
+	print(file)
+	subprocess.call([cmd, file])
